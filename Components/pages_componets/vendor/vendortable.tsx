509ce44992d2c49fb0455b383296544505f5b5eb
Harsh Aggarwal <87479663+HARSHaggarwal01@users.noreply.github.com>
2024-07-12 23:50:00 +0530

    changes done

--- Components/pages_componets/vendor/vendortable.tsx
+++ Components/pages_componets/vendor/vendortable.tsx
@@ -1,22 +1,28 @@
 import { selected_is_success } from "Components/slices/membership/reducer";
-import { GetMembership } from "Components/slices/membership/thunk";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { Card, Col, Row ,Table } from "react-bootstrap";
+import { Card, Col, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import {
   GET_ALL_VENDORS,
-  baseURL
+  baseURL,
 } from "../../../Components/helpers/url_helper";
 
+// Define Vendor interface
+interface Vendor {
+  _id: string;
+  name: string;
+  email: string;
+  mobile: string;
+}
 
-const VendorTable = () => {
+const VendorTable: React.FC = () => {
   const router = useRouter();
   const dispatch = useDispatch();
 
-  const [vendors,setVendors] = useState([]);
-  console.log(vendors,"frjfj");
+  const [vendors, setVendors] = useState<Vendor[]>([]); // Specify the type for vendors
+  console.log(vendors, "frjfj");
 
   const { data } = useSelector((state: any) => ({
     data: state.membership.data,
@@ -36,8 +42,6 @@ const VendorTable = () => {
     vendorlist();
   }, []);
 
-  
-  // Concatenate static default entry with data or use it if data is empty
   return (
     <Col xl={12}>
       <Card>
@@ -53,25 +57,15 @@ const VendorTable = () => {
                   <th>Name</th>
                   <th>Email</th>
                   <th>Mobile</th>
-                  {/* <th>Actions</th> */}
                 </tr>
               </thead>
               <tbody>
-                {vendors.map((vendor,index) => (
-                  <tr key={index+1}>
-                    <td>{index+1}</td>
+                {vendors.map((vendor, index) => (
+                  <tr key={vendor._id}>
+                    <td>{index + 1}</td>
                     <td>{vendor.name}</td>
                     <td>{vendor.email}</td>
                     <td>{vendor.mobile}</td>
-                    {/* <td>
-                      <i
-                        className="bi bi-pen cursor-pointer"
-                        onClick={() => {
-                          router.push(`/vendor/edit-vendor/${vendor._id}`);
-                          dispatch(selected_is_success(vendor));
-                        }}
-                      ></i>
-                    </td> */}
                   </tr>
                 ))}
               </tbody>
