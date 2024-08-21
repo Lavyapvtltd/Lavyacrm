ea9ef16c252088ca0054643895e3f72dadb7e698
Harsh Aggarwal <87479663+HARSHaggarwal01@users.noreply.github.com>
2024-08-21 15:42:18 +0530

    ssl changes

--- Components/helpers/url_helper.ts
+++ Components/helpers/url_helper.ts
@@ -3,8 +3,8 @@
 // export const imagebaseURL = "http://18.204.165.219:9291/resources/";
 
 // LIVE;
-export const baseURL = "https://api.lavyacompany.com/api/v1";
-export const imagebaseURL = "https://api.lavyacompany.com/resources/";
+export const baseURL = "http://api.lavyacompany.com/api/v1";
+export const imagebaseURL = "http://api.lavyacompany.com/resources/";
 
 // LOCAL;
 // export const baseURL = "http://192.168.0.177:9291/api/v1";
